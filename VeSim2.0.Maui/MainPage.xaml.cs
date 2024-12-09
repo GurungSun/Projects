12ec5b4579fc7d75e8a1fbaa48d73135d527cbd4
sunde <sunde@SUNDEEP>
2024-12-09 23:05:28 +0000

    new update

--- VeSim2.0.Maui/MainPage.xaml.cs
+++ VeSim2.0.Maui/MainPage.xaml.cs
@@ -1,12 +1,17 @@
-﻿namespace VeSim2._0.Maui
+﻿using VeSim2._0.Maui.Models;
+using VeSim2._0.Maui.ViewModels;
+
+namespace VeSim2._0.Maui
 {
     public partial class MainPage : ContentPage
     {
         
-
+        
         public MainPage()
         {
             InitializeComponent();
+
+            
         }
         
 
